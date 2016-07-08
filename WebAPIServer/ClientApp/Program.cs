a0bd0dafaa27e0d88470098863790f26788e4675
Simon <Simon@DESKTOP-RI67981>
2016-07-08 16:51:26 +0200

    Init commit ServerApp with basic api Client App

---
+++ WebAPIServer/ClientApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using SelfHostedWebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+            client.BaseAddress = new Uri("http://localhost:8080");
+
+            ListAllUsers();
+            UserById(1);
+
+            Console.WriteLine("Press Enter to quit.");
+            Console.ReadLine();
+        }
+
+
+        static void ListAllUsers()
+        {
+            HttpResponseMessage resp = client.GetAsync("api/basic").Result;
+            resp.EnsureSuccessStatusCode();
+
+            var products = resp.Content.ReadAsAsync<IEnumerable<User>>().Result;
+            foreach (var p in products)
+            {
+                Console.WriteLine("{0} {1} {2}", p.Id, p.Name, p.Age);
+            }
+        }
+
+        static void UserById(int id)
+        {
+            var resp = client.GetAsync(string.Format("api/basic/{0}", id)).Result;
+            resp.EnsureSuccessStatusCode();
+
+            var product = resp.Content.ReadAsAsync<User>().Result;
+            Console.WriteLine("ID {0}: {1}", id, product.Name);
+        }
+    }
+}
---
+++ WebAPIServer/SelfHostedWebApi/BasicController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SelfHostedWebApi
+{
+    public class BasicController: ApiController
+    {
+        User[] users = new User[]
+        {
+            new User(1,"simon", 26),
+            new User(2,"simon27", 27),
+            new User(3,"simon28", 28)
+        };
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return users;
+        }
+
+        public User GetUserById(int id)
+        {
+            var result = users.FirstOrDefault(u => u.Id == id);
+            if (result == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return result;
+        }
+    }
+}
